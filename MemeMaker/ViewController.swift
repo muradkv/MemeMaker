1b85725e79639458f400a826ed4ab6222f2ccb63
murad <muradkv@gmail.com>
2019-07-13 22:37:16 +0300

    Initial commit

---
+++ MemeMaker/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MemeMaker
+//
+//  Created by murad on 12/07/2019.
+//  Copyright © 2019 murad. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
