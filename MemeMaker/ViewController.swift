321fee179b842afa2cc95333b9353533cfea2307
murad <muradkv@gmail.com>
2019-07-15 23:53:14 +0300

    Add UI: labels, segmented controls and imageView

--- MemeMaker/ViewController.swift
+++ MemeMaker/ViewController.swift
@@ -9,12 +9,20 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet var topCaptionSegmentedControl: UISegmentedControl!
+    @IBOutlet var bottomCaptionSegmentedControl: UISegmentedControl!
+    @IBOutlet var topCaptionLabel: UILabel!
+    @IBOutlet var bottomCaptionLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-
+    @IBAction func segmentedControlsTapped(_ sender: UISegmentedControl) {
+        
+    }
+    
 }
 
